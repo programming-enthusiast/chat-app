09dd9110791c301634f7e886aadd467600cf00a9
programming-enthusiast <w.technology@outlook.com>
2021-04-05 06:30:29 +0800

    Fix issues

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,7 +12,126 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Route::middleware('api')->group(function ($router) {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// });
+
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
+Route::post('/logout', 'AuthController@logout');
+
+
+Route::get('/user', 'AuthController@user');
+
+
+
+
+
+
+
+Route::group([
+    'middleware' => 'auth:api'
+], function () {
+    /*
+    * This is the main app route [Chat Messenger]
+    */
+    Route::get('/chat', 'MessagesController@index')->name(config('chat.path'));
+
+    /**
+     *  Fetch info for specific id [user/group]
+     */
+    Route::post('/chat/idInfo', 'MessagesController@idFetchData');
+
+    /**
+     * Send message route
+     */
+    Route::post('/chat/sendMessage', 'MessagesController@send')->name('send.message');
+
+    /**
+     * Fetch messages 
+     */
+    Route::post('/chat/fetchMessages', 'MessagesController@fetch')->name('fetch.messages');
+
+    /**
+     * Download attachments route to create a downloadable links
+     */
+    Route::get('/chat/download/{fileName}', 'MessagesController@download')->name(config('chat.attachments.route'));
+
+    /**
+     * Authintication for pusher private channels
+     */
+    Route::post('/chat/chat/auth', 'MessagesController@pusherAuth')->name('pusher.auth');
+
+    /**
+     * Make messages as seen
+     */
+    Route::post('/chat/makeSeen', 'MessagesController@seen')->name('messages.seen');
+
+    /**
+     * Get contacts
+     */
+    Route::post('/chat/getContacts', 'MessagesController@getContacts')->name('contacts.get');
+
+    /**
+     * Update contact item data
+     */
+    Route::post('/chat/updateContacts', 'MessagesController@updateContactItem')->name('contacts.update');
+
+
+    /**
+     * Star in favorite list
+     */
+    Route::post('/chat/star', 'MessagesController@favorite')->name('star');
+
+    /**
+     * get favorites list
+     */
+    Route::post('/chat/favorites', 'MessagesController@getFavorites')->name('favorites');
+
+    /**
+     * Search in messenger
+     */
+    Route::post('/chat/search', 'MessagesController@search')->name('search');
+
+    /**
+     * Get shared photos
+     */
+    Route::post('/chat/shared', 'MessagesController@sharedPhotos')->name('shared');
+
+    /**
+     * Delete Conversation
+     */
+    Route::post('/chat/deleteConversation', 'MessagesController@deleteConversation')->name('conversation.delete');
+
+    /**
+     * Delete Conversation
+     */
+    Route::post('/chat/updateSettings', 'MessagesController@updateSettings')->name('avatar.update');
+
+    /**
+     * Set active status
+     */
+    Route::post('/chat/setActiveStatus', 'MessagesController@setActiveStatus')->name('activeStatus.set');
+
+
+    /*
+    * user view by id.
+    * Note : If you added routes after the [User] which is the below one,
+    * it will considered as user id.
+    *
+    * e.g. - The commented routes below :
+    */
+    Route::get('/{id}', 'MessagesController@index')->name('user');
+
 });
+
+
+
+
+
+
+Route::get('/', 'HomeController@index');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
